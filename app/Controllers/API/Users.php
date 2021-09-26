958ab4d5163bc0cac762bcf7622cd3cb9251fb35
Natan Felles <natanfelles@gmail.com>
2021-09-26 01:01:13 -0300

    Update API Users resource controller

--- app/Controllers/API/Users.php
+++ app/Controllers/API/Users.php
@@ -5,19 +5,16 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace App\Controllers;
+namespace App\Controllers\API;
 
 use App\Models\Users as UsersModel;
 use Framework\Crypto\Password;
 use Framework\HTTP\Request;
 use Framework\HTTP\Response;
-use Framework\MVC\Controller;
 use Framework\MVC\ModelInterface;
 use Framework\Pagination\Pager;
 use Framework\Routing\Attributes\Route;
 use Framework\Routing\ResourceInterface;
-use JetBrains\PhpStorm\ArrayShape;
-use JetBrains\PhpStorm\Pure;
 
 /**
  * Class Users.
@@ -35,7 +32,7 @@ final class Users extends Controller implements ResourceInterface
     /**
      * @return array<string,mixed>
      */
-    #[Route('GET', '/users', name: 'users.index')]
+    #[Route('GET', '/users', name: 'api.users.index')]
     public function index() : array
     {
         $page = $this->request->getGet('page') ?? 1;
@@ -48,11 +45,10 @@ final class Users extends Controller implements ResourceInterface
                 $entity->updatedAt
             );
         }
-        return [
-            'status' => $this->getStatus(),
+        return $this->respond([
             'data' => $entities,
             'links' => $this->model->getPager(),
-        ];
+        ]);
     }
 
     /**
@@ -60,56 +56,44 @@ final class Users extends Controller implements ResourceInterface
      *
      * @return array<string,mixed>
      */
-    #[Route('GET', '/users/{int}', name: 'users.show')]
+    #[Route('GET', '/users/{int}', name: 'api.users.show')]
     public function show(string $id) : array
     {
         $entity = $this->model->find($id);
         if ($entity) {
-            return [
-                'status' => $this->getStatus(),
+            return $this->respond([
                 'data' => $entity,
-            ];
+            ]);
         }
-        $this->response->setStatus(Response::CODE_NOT_FOUND);
-        return [
-            'status' => $this->getStatus(),
-        ];
+        return $this->respondNotFound();
     }
 
     /**
      * @return array<string,mixed>
      */
-    #[Route('POST', '/users', name: 'users.create')]
+    #[Route('POST', '/users', name: 'api.users.create')]
     public function create() : array
     {
         $data = $this->request->isJson()
             ? $this->request->getJson(true)
             : $this->request->getPost();
         if ($data === false) {
-            $this->response->setStatus(Response::CODE_UNPROCESSABLE_ENTITY);
-            return [
-                'status' => $this->getStatus(),
-            ];
+            return $this->respond(code: Response::CODE_UNPROCESSABLE_ENTITY);
         }
         if (isset($data['password']) && \is_string($data['password'])) {
             $data['password'] = Password::hash($data['password']);
         }
         $id = $this->model->create($data);
         if ($id === false) {
-            return $this->respondErrors();
+            return $this->respondErrors($this->model->getErrors());
         }
         $entity = $this->model->find($id);
         if ($entity) {
-            $this->response->setStatus(Response::CODE_CREATED);
-            return [
-                'status' => $this->getStatus(),
+            return $this->respond([
                 'data' => $entity,
-            ];
+            ], Response::CODE_CREATED);
         }
-        $this->response->setStatus(Response::CODE_PROCESSING);
-        return [
-            'status' => $this->getStatus(),
-        ];
+        return $this->respond(code: Response::CODE_PROCESSING);
     }
 
     /**
@@ -117,24 +101,18 @@ final class Users extends Controller implements ResourceInterface
      *
      * @return array<string,mixed>
      */
-    #[Route('PATCH', '/users/{int}', name: 'users.update')]
+    #[Route('PATCH', '/users/{int}', name: 'api.users.update')]
     public function update(string $id) : array
     {
         $data = $this->request->isJson()
             ? $this->request->getJson(true)
-            : $this->request->getPost();
+            : $this->request->getParsedBody();
         if ($data === false) {
-            $this->response->setStatus(Response::CODE_UNPROCESSABLE_ENTITY);
-            return [
-                'status' => $this->getStatus(),
-            ];
+            return $this->respond(code: Response::CODE_UNPROCESSABLE_ENTITY);
         }
         $entity = $this->model->find($id);
         if ( ! $entity) {
-            $this->response->setStatus(Response::CODE_NOT_FOUND);
-            return [
-                'status' => $this->getStatus(),
-            ];
+            return $this->respondNotFound();
         }
         if (isset($data['email']) && $data['email'] === $entity->email) {
             unset($data['email']);
@@ -144,20 +122,15 @@ final class Users extends Controller implements ResourceInterface
         }
         $affectedRows = $this->model->update($id, $data);
         if ($affectedRows === false) {
-            return $this->respondErrors();
+            return $this->respondErrors($this->model->getErrors());
         }
         $entity = $this->model->find($id);
         if ($entity) {
-            $this->response->setStatus(Response::CODE_OK);
-            return [
-                'status' => $this->getStatus(),
+            return $this->respond([
                 'data' => $entity,
-            ];
+            ]);
         }
-        $this->response->setStatus(Response::CODE_PROCESSING);
-        return [
-            'status' => $this->getStatus(),
-        ];
+        return $this->respond(code: Response::CODE_PROCESSING);
     }
 
     /**
@@ -165,19 +138,16 @@ final class Users extends Controller implements ResourceInterface
      *
      * @return array<string,mixed>
      */
-    #[Route('PUT', '/users/{int}', name: 'users.replace')]
+    #[Route('PUT', '/users/{int}', name: 'api.users.replace')]
     public function replace(string $id) : array
     {
-        $this->response->setStatus(Response::CODE_METHOD_NOT_ALLOWED)
-            ->setHeader(Response::HEADER_ALLOW, \implode(', ', [
-                Request::METHOD_DELETE,
-                Request::METHOD_GET,
-                Request::METHOD_HEAD,
-                Request::METHOD_PATCH,
-            ]));
-        return [
-            'status' => $this->getStatus(),
-        ];
+        $this->response->setHeader(Response::HEADER_ALLOW, \implode(', ', [
+            Request::METHOD_DELETE,
+            Request::METHOD_GET,
+            Request::METHOD_HEAD,
+            Request::METHOD_PATCH,
+        ]));
+        return $this->respond(code: Response::CODE_METHOD_NOT_ALLOWED);
     }
 
     /**
@@ -185,46 +155,14 @@ final class Users extends Controller implements ResourceInterface
      *
      * @return array<string,mixed>
      */
-    #[Route('DELETE', '/users/{int}', name: 'users.delete')]
+    #[Route('DELETE', '/users/{int}', name: 'api.users.delete')]
     public function delete(string $id) : array
     {
         $entity = $this->model->find($id);
         if ( ! $entity) {
-            $this->response->setStatus(Response::CODE_NOT_FOUND);
-            return [
-                'status' => $this->getStatus(),
-            ];
+            return $this->respondNotFound();
         }
         $this->model->delete($id);
-        $this->response->setStatus(Response::CODE_OK);
-        return [
-            'status' => $this->getStatus(),
-        ];
-    }
-
-    /**
-     * @return array<string,mixed>
-     */
-    #[Pure]
-    #[ArrayShape(['code' => 'int', 'reason' => 'string'])]
-    protected function getStatus() : array
-    {
-        return [
-            'code' => $this->response->getStatusCode(),
-            'reason' => $this->response->getStatusReason(),
-        ];
-    }
-
-    /**
-     * @return array<string,mixed>
-     */
-    #[ArrayShape(['status' => 'array', 'errors' => 'string[]'])]
-    protected function respondErrors() : array
-    {
-        $this->response->setStatus(Response::CODE_BAD_REQUEST);
-        return [
-            'status' => $this->getStatus(),
-            'errors' => $this->model->getErrors(),
-        ];
+        return $this->respond();
     }
 }
