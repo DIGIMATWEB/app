3199de6ef59edbe7b19dfde9b221fddb764179f0
Natan Felles <natanfelles@gmail.com>
2021-09-26 01:07:43 -0300

    Serve API routes

--- extra/routes.php
+++ extra/routes.php
@@ -5,9 +5,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+use Framework\HTTP\Response;
 use Framework\Routing\RouteCollection;
 
-App::router()->serve(null, static function (RouteCollection $routes) : void {
+App::router()->serve('http://localhost:8080', static function (RouteCollection $routes) : void {
     $routes->namespace('App\Controllers', [
         $routes->get('/', 'Home::index', 'home'),
         $routes->resource('/users', 'Users', 'users', ['replace']),
@@ -15,4 +16,17 @@ App::router()->serve(null, static function (RouteCollection $routes) : void {
     $routes->notFound(static function () {
         return not_found();
     });
-});
+})->serve('http://localhost:8081', static function (RouteCollection $routes) : void {
+    $routes->namespace('App\Controllers\API', [
+        $routes->get('/', 'Home', 'home'),
+        $routes->resource('/users', 'Users', 'users', ['replace']),
+    ]);
+    $routes->notFound(static function ($args, $req, Response $res) {
+        return [
+            'status' => [
+                'code' => $res->getStatusCode(),
+                'reason' => $res->getStatusReason(),
+            ],
+        ];
+    });
+}, 'api');
